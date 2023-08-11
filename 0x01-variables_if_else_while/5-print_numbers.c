a72ca22a9abb82071cea07d3d7237d3fb67f26ce
BildadM <billymshemi@gmail.com>
2023-08-11 12:35:35 -0700

    1-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here
+ *
+ * Return: zero if successful
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0 ; b < 10 ; b++)
+		printf("%d", b);
+	printf("\n");
+	return (0);
+}
