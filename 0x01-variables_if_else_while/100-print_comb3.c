1699674f5c972db730f3597dd99a675befe24636
BildadM <billymshemi@gmail.com>
2023-08-11 15:31:29 -0700

    two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: zero if successful
+ */
+
+int main(void)
+{
+	int a = '0';
+	int b = '1';
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			if (!(a > b) || a == b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		b = '0';
+		a++;
+	}
+	return (0);
+}
