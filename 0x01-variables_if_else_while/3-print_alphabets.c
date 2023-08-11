8e773414a3d528e46451f4bdff87f8c01f0f232d
BildadM <billymshemi@gmail.com>
2023-08-11 12:06:53 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here.
+ * for - sets condition for the loop
+ *
+ * Return: zero if successful
+ */
+
+int main(void)
+{
+        char b;
+
+        for (b = 'a' ; b <= 'z' ; b++)
+                putchar(b);
+	for (b = 'A' ; b <= 'Z' ; b++)
+		putchar(b);
+        putchar('\n');
+        return (0);
+}
