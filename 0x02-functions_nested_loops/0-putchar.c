763138dbb7a60957457f6501f44536ad8e79b4d8
BildadM <billymshemi@gmail.com>
2023-08-15 03:38:42 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - start of the program
+ * _putchar - prints the desired character
+ *
+ * Return: zero if succesful
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
