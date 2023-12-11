68156d98ef4097afcc441edc5e93c8380ea614a4
BildadM <bildadmuchemi.w@gmail.com>
2023-12-11 13:24:46 -0800

    python functions

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * sum - sum two numbers
+ * @a: int one
+ * @b: int two
+ *
+ * Return: sum
+ */
+
+int sum(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: int one
+ * @b: int two
+ *
+ * Return: minus
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul two numbers
+ * @a: int one
+ * @b: int two
+ *
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div two numbers
+ * @a: int one
+ * @b: int two
+ *
+ * Return: div
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod of two numbers
+ * @a: int one
+ * @b: int two
+ *
+ * Return: mod
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
